62e6da9d3ca1907538341de0e8dba5ac0b21b48e
Wen Bin <116337818+Laizhen27@users.noreply.github.com>
2022-11-28 12:27:39 +0800

    added topup

--- Hero.js
+++ Hero.js
@@ -1,15 +1,19 @@
-Fiterselection("All");
-function filterCategory(c) {
-  var x, i;
-  x = document.getElementsByClassName("image");
-  if (c == "all") c = "";
-  for (i = 0; i < x.length; i++) {
-    removeClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
-  }
+$(document).on('scroll',function(){
+  $('h2').css("left",Math.max(500 - 0.35* window.scrollY)+"px")
 }
 
-function RemoveClass(element, name) {
-  visualViewport, arr1, arr2;
-  arr;
-}
+// Fiterselection("All");
+// function filterCategory(c) {
+//   var x, i;
+//   x = document.getElementsByClassName("image");
+//   if (c == "all") c = "";
+//   for (i = 0; i < x.length; i++) {
+//     removeClass(x[i], "show");
+//     if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
+//   }
+// }
+
+// function RemoveClass(element, name) {
+//   visualViewport, arr1, arr2;
+//   arr;
+// }
